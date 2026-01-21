87315d7586d713580a8a7bf95a731b289bb13310
Filip Hracek <filip.hracek@gmail.com>
2026-01-21 21:34:09 +0100

    Add T341 prep run

---
+++ t341_prep_run.jl
@@ -0,0 +1,38 @@
+# Shared configuration
+
+# Lower resolution for faster test runs (e.g., 85, 127, 170, 255, 341 .. 1365).
+# https://speedyweather.github.io/SpeedyWeatherDocumentation/dev/spectral_transform/#Available-horizontal-resolutions
+const TRUNC = 341
+const NLAYERS = 8
+
+# The "warming" constant. Solar is normally 1365.0.
+const SOLAR_CONSTANT = 1365.0 
+
+
+# Simulation settings
+const RUN_PERIOD = Day(365)
+const OUTPUT_TIMESTEP = Hour(2)
+const OUTPUT_DIR = "T341"    # A directory to store the results.
+const RUN_ID = "initial"
+const START_DATE = DateTime(2026, 5, 1)
+
+# Whether or not this simulation should start from the end of a previous run.
+const CONTINUATION = false
+
+# The location of the previous run. That run must be compatible with this one
+# (i.e. TRUNC and NLAYERS must be the same).
+const CONTINUATION_DIR = "test1"
+const CONTINUATION_ID = "a"
+const CONTINUATION_RUN_NUMBER = 1
+
+
+# Animation settings
+const VARIANT = "contrast"
+const ANIM_INPUT_DIR = "T341/run_initial_0001"
+const LEVEL_LOW = 1
+const LEVEL_HIGH = NLAYERS - 1
+const FRAME_RATE = 15
+const PROJECTION = "+proj=ortho +lon_0=0 +lat_0=30"
+const COLOR_RANGE = (-20, 20)  # Celsius.
+const COLORMAP = :coolwarm  # :thermal, :coolwarm, :viridis, :plasma, :inferno, :magma, :cividis, :greens, :blues, :reds
+const FIGURE_SIZE = (1920, 1080)
